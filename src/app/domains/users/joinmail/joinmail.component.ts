ffba015460bb4bcfbc3bf43abdaf78f2fdc87766
Martin Mendez <martinpercu@gmail.com>
2024-02-23 15:56:12 -0300

    emailJS implementation

--- src/app/domains/users/joinmail/joinmail.component.ts
+++ src/app/domains/users/joinmail/joinmail.component.ts
@@ -4,6 +4,9 @@ import { Router } from '@angular/router';
 import { ClientService } from '@services/client.service';
 import { Client } from '@models/client.model'
 
+import { EmailService } from '@services/email.service';
+
+
 
 @Component({
   selector: 'app-joinmail',
@@ -14,15 +17,11 @@ import { Client } from '@models/client.model'
 })
 export class JoinmailComponent {
 
-  // emailjs.send("service_4wbv4ud","template_n5608jq",{
-  //   fullname: "ClienteFullname",
-  //   client_email: "order@4timp.com",
-  //   });
-
   private clientService = inject(ClientService);
   private router = inject(Router);
 
   private formBuilder = inject(FormBuilder)
+  private emailsender = inject(EmailService)
 
   client!: Client;
 
@@ -44,7 +43,21 @@ export class JoinmailComponent {
     const response = await this.clientService.addClient(this.formJoinMail.value);
     console.log(response);
     this.navToJoined();
+    // const form = this.formJoinMail.value;
+    this.emailsender.sendEmail(this.formJoinMail.value);
+    // this.sendEmail()
   }
+
+  // private async sendEmail() {
+  //   emailjs.init('WBAY4V6yXv5MXO115');
+  //   console.log(this.formJoinMail.value);
+  //   let response = await emailjs.send("service_4wbv4ud","template_n5608jq",{
+  //     fullname: this.formJoinMail.value.fullname,
+  //     client_email: this.formJoinMail.value.email,
+  //     });
+
+  //     alert('message send it OK')
+  //  }
 
   navToJoined() {
     this.router.navigate(['joinedmaillist'])
---
+++ src/app/services/email.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import emailjs from '@emailjs/browser';
+
+// import
+
+import { emailjsInitVar } from './../../environment/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+
+  form: any;
+
+  constructor() {
+  }
+
+
+  async sendEmail(form: any) {
+    emailjs.init(emailjsInitVar);
+    console.log(form);
+    let response = await emailjs.send("service_4wbv4ud", "template_n5608jq", {
+      fullname: form.fullname,
+      client_email: form.email,
+    });
+    alert('message send it OK NO')
+    console.log(response);
+  }
+
+}
