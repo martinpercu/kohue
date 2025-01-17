493c528a14e7c2d4094be4498682f5f7cdbf62ee
martinpercu <97993590+martinpercu@users.noreply.github.com>
2025-01-17 11:20:40 -0500

    review of history to stripe ordering customer by stripe in admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { AgegateComponent } from '@shared/agegate/agegate.component'
+import { AgegateComponent } from '@shared/agegate/agegate.component';
+import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,14 @@ import { AgegateComponent } from '@shared/agegate/agegate.component'
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'maxwines';
+  title = environment.TITLE_INDEX;
+  // constructor()  {
+  //   if(environment.REDIRECT_TO_KOHUE) {
+  //     // alert("Estamos para ir a KOHUE");
+  //     window.location.href = 'https://kohuewines.com';
+  //   }
+  //   if(!environment.REDIRECT_TO_KOHUE) {
+  //     alert("Estamos En KOHUE");
+  //   }
+  // }
 }
